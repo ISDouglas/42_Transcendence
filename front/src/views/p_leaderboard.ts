a1dba87c1329f6688f30731641834a5c3911001b
Noah Rontard <rontard.noah@gmail.com>
2026-01-16 18:58:42 +0100

    need correct index

--- front/src/views/p_leaderboard.ts
+++ front/src/views/p_leaderboard.ts
@@ -16,19 +16,19 @@ export async function InitLeaderboard()
 	{
 		(document.getElementById("avatar-1") as HTMLImageElement).src = leaderboard.InfoUsers[0].avatar;
 		(document.getElementById("pseudo-1") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[0].pseudo;
-		(document.getElementById("elo-1") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[0].elo.toString();
+		(document.getElementById("elo-1") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[0].elo.toString() + " 🥐";
 	}
 	if (leaderboard.InfoUsers.length > 1)
 	{
 		(document.getElementById("avatar-2") as HTMLImageElement).src = leaderboard.InfoUsers[1].avatar;
 		(document.getElementById("pseudo-2") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[1].pseudo;
-		(document.getElementById("elo-2") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[1].elo.toString();
+		(document.getElementById("elo-2") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[1].elo.toString() + " 🥐";
 	}
 	if (leaderboard.InfoUsers.length > 2)
 	{
 		(document.getElementById("avatar-3") as HTMLImageElement).src = leaderboard.InfoUsers[2].avatar;
 		(document.getElementById("pseudo-3") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[2].pseudo;
-		(document.getElementById("elo-3") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[2].elo.toString();
+		(document.getElementById("elo-3") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[2].elo.toString() + " 🥐";
 	}
 	for (let i = 3; i < 50; i++)
 	{
@@ -38,10 +38,24 @@ export async function InitLeaderboard()
 		{
 			(li.getElementById("avatar") as HTMLImageElement).src = leaderboard.InfoUsers[i].avatar;
 			(li.getElementById("pseudo") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[i].pseudo;
-			(li.getElementById("elo") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[i].elo.toString();
+			(li.getElementById("elo") as HTMLParagraphElement).textContent = leaderboard.InfoUsers[i].elo.toString() + " 🥐";
+			if (leaderboard.user.pseudo === leaderboard.InfoUsers[i].pseudo)
+			{
+				(li.getElementById("background") as HTMLDivElement).classList.remove("bg-linear-to-r", "from-amber-50", "via-orange-50", "to-yellow-50");
+				(li.getElementById("background") as HTMLDivElement).classList.add("bg-linear-to-r", "from-amber-100", "via-orange-100", "to-yellow-100");
+			}
 		}
 		(li.getElementById("position") as HTMLParagraphElement).textContent = "#" + (i+1).toString();
 		container.appendChild(li);
 	}
+	if (leaderboard.InfoUsers.length >= 50 && leaderboard.user.elo < leaderboard.InfoUsers[49].elo)
+	{
+		(document.getElementById("your-avatar") as HTMLImageElement).src = leaderboard.user.avatar;
+		(document.getElementById("your-pseudo") as HTMLParagraphElement).textContent = leaderboard.user.pseudo;
+		(document.getElementById("your-elo") as HTMLParagraphElement).textContent = leaderboard.user.elo.toString() + " 🥐";
+	}
+	else
+		(document.getElementById("your-position") as HTMLDivElement).classList.add("hidden");
+
 	console.log(leaderboard);
 }
